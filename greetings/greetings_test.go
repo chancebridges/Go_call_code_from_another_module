506b1658bf42c69f5891d837d8cb6a7ec5bdb717
Chance Bridges <chances.bridges1998@gmail.com>
2022-08-16 15:48:09 -0400

    updated push

---
+++ greetings/greetings_test.go
@@ -0,0 +1,13 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+// TestHelloName calls greetings.Hello with a name, checking
+// for a valid return value.
+func TestHelloName(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+}
